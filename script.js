14c54852226132fada4d713a5a2bab5e951da912
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-05-17 18:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let mp = new Map();
+	for(let ch of str){
+		mp.set(ch, (mp.get(ch)||0)+1);
+	}
+	for(let ch of str){
+		if(mp.get(ch)===1)return ch;
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
